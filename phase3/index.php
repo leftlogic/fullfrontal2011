06a717fbc19400b621c2f44b30e8f10b0a216e45
Jon Linklater-Johnson <jon@binarytales.co.uk>
2011-06-27 10:51:04 +0100

    HTML marked up for first batch of pages

--- phase3/index.php
+++ phase3/index.php
@@ -39,19 +39,58 @@ if (isset($_POST['email']) && $_POST['email'] && validEmail($_POST['email'])) {
   <div class="wrapper">
     <header>
       <h1 class="full-frontal">Full Frontal</h1> 
-      <time datetime="2011-11-11T09:00:00Z">11.11.11</time>
+
       <h2>JavaScript <br /> <span>Conference</span></h2>
-      <h3>Duke Of Yorks, Brighton, <br /> 11th November 2011</h3>
+      <h3>Duke Of Yorks, Brighton, <br /> <time datetime="2011-11-11T09:00:00Z">11th November 2011</time></h3>
+    
+      <a href="" class="tickets">
+        Earlybird <br />
+        1 dat &pound;129+ <abbr>VAT</abbr> <br />
+        <span>Buy tickets</span>
+      </a>
     </header>
     
-    <form action="/" method="post">
-      <label for="email">Enter your email address below to get updated on Full Frontal 2011</label>
-      <p>
-        <input type="email" id="email" name="email" placeholder="Enter your email for updates..." />
-        <button type="submit"><span>Go</span></button>
-      </p>
-    </form>
+    <nav>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Speakers</a></li>
+        <li><a href="">Schedule</a></li>
+        <li><a href="">The Venue</a></li>
+        <li><a href="">Sponsors</a></li>
+      </ul>
+    </nav>
     
+    <hgroup>
+      <h1>Full Frontal</h1>
+      <h2>Javascript Conference<h2>
+    </hgroup>
+    
+    <div class="list-speakers">    
+      <ul>
+        <li><a href="">Paul Rouget</a></li>
+        <li><a href="">Brian Leroux</a></li>
+        <li><a href="">Dan Webb</a></li>
+        <li><a href="">Alex Russell</a></li>
+        <li><a href="">Jan Lehnardt</a></li>
+        <li><a href="">Seb Lee-Delisle</a></li>
+        <li><a href="">Paul Bakhaus</a></li>
+      </ul>
+    
+      <h3>Duke Of Yorks, Brighton, <br /> 11th November 2011</h3>
+    
+      <div class="image-cube">
+        <img src="" width="" height="" alt="" />
+        <img src="" width="" height="" alt="" />
+        <img src="" width="" height="" alt="" />
+        <img src="" width="" height="" alt="" />
+        <img src="" width="" height="" alt="" />
+        <img src="" width="" height="" alt="" />
+        <img src="" width="" height="" alt="" />
+        <img src="" width="" height="" alt="" />
+        <img src="" width="" height="" alt="" />
+      </div>
+    </div>
+
     <div class="want-to">
       <section class="speak">
         <h1>Want to<br />speak?</h1>
@@ -111,7 +150,100 @@ for ($i = 0; $i < 5; $i++) : $fav = $favs[$i]; ?>
 <?php endfor ?>
       
       <div class="clear"></div>
-    </div>
+    </div><!-- .tweets -->
     
-  </div>
+    <h1>Schedule</h1>
+    
+    <ol class="schedule">
+      <li>
+        <span class="timeslot">
+          <time>09:00</time> &emdash; <time></time>
+        </span>
+        <section>
+          <h1>Registration<h1>
+        </section>
+      </li>
+      
+      <li>
+        <span class="timeslot">
+          <time>09:00</time> &emdash; <time></time>
+          <a href="">Alex Russel</a>
+        </span>
+        <img src="" width="" height="" alt="" />
+        <section>
+          <h1>Less and more: How HTML5 will change the framework landscape<h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </section>
+      </li>
+    </ol>
+    
+    <h1>Speakers</h1>
+    
+    <section>
+      <h1><span class="number"></span>Brendan Dawes</h1>
+      <img src="" width="" height="" alt="" />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
+      </p>
+    </section>
+    
+    <h1>Sponsors</h1>
+    
+    <h2>
+      Full Frontal
+      is proudly
+      sponsored by
+    </h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    
+    <section class="sponsor full">
+      <a href=""><h1><img src="" width="" height="" alt="Mozilla" /></h1></a>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </section>
+    
+    <section class="sponsor half">
+      <h1>Half Monties
+        <a href="">Become a Half Monty sponsor</a>  
+      </h1>
+      <a href=""><img src="" width="" height="" alt="" /></a>
+      <a href=""><img src="" width="" height="" alt="" /></a>
+      <a href=""><img src="" width="" height="" alt="" /></a>
+    </section>
+    
+    <section class="sponsor small">
+      <h1>Small Monties
+        <a href="">Become a Small Monty Sponsor</a>
+      <h1>
+        <a href=""><img src="" width="" height="" alt="" /></a>
+        <a href=""><img src="" width="" height="" alt="" /></a>
+        <a href=""><img src="" width="" height="" alt="" /></a>
+        <a href=""><img src="" width="" height="" alt="" /></a>
+        <a href=""><img src="" width="" height="" alt="" /></a>
+        <a class="available" href=""><img src="" width="" height="" alt="" /></a>
+    </section>
+    
+    <section class="sponsor media">
+      <h1>Media Partners
+        <a href="">Become a Media Partner</a>
+      </h1>
+        <a href=""><img src="" width="" height="" alt="" /></a>
+        <a href=""><img src="" width="" height="" alt="" /></a>
+        <a href=""><img src="" width="" height="" alt="" /></a>
+        <a class="available" href=""><img src="" width="" height="" alt="" /></a>
+        <a class="available" href=""><img src="" width="" height="" alt="" /></a>
+        <a class="available" href=""><img src="" width="" height="" alt="" /></a>
+    </section>
+    
+   
+    
+  </div><!-- .wrapper -->
 <?php include('footer.php'); ?>
