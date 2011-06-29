d797f5925a456ffe28782498120cf87403200d64
Jon Linklater-Johnson <jon@binarytales.co.uk>
2011-06-29 18:04:33 +0100

    Tackling low hanging fruit and the home, tickets and workshop pages

--- phase3/working.php
+++ phase3/working.php
@@ -45,21 +45,21 @@ if (isset($_POST['email']) && $_POST['email'] && validEmail($_POST['email'])) {
         <h3>Duke Of Yorks, Brighton, <br> <time datetime=2011-11-11T09:00:00Z>11th November 2011</time></h3>
       </hgroup>
     
-      <a href=# class="tickets early-bird">
+      <a href=# class="buy-tickets early-bird">
         Earlybird<br>
         <span>1 day &pound;99+ <abbr>VAT</abbr></span><br>
-        <span class=button><img src=images/buy-tickets width=278 height=48 alt="Buy Tickets"></span>
+        <span class=buy-button><img src=images/buy-tickets width=278 height=48 alt="Buy Tickets"></span>
       </a>
       
       <!--
       
-      <a href=# class="tickets buy-now">
+      <a href=# class="buy-tickets buy-now">
         Buy Now<br>
         <span>1 day &pound;129+ <abbr>VAT</abbr></span><br>
-        <span class=button><img src=images/buy-tickets width=278 height=48 alt="Buy Tickets"></span>
+        <span class=buy-button><img src=images/buy-tickets width=278 height=48 alt="Buy Tickets"></span>
       </a>
             
-      <span class="tickets sold-out">Sold Out</span>
+      <span class="buy-tickets sold-out">Sold Out</span>
       
       -->
       
@@ -82,12 +82,9 @@ if (isset($_POST['email']) && $_POST['email'] && validEmail($_POST['email'])) {
     
     
     
-    <hgroup>
-      <h1 class=headline>Full Frontal</h1>
-      <h2 class=headline>Javascript Conference</h2>
-    </hgroup>
+    <h1 class=headline><img src=images/headlines/full-frontal-javascript-conference.png alt="Full Frontal Javascript Conference"></h1>
     
-    <div class=list-speakers>    
+    <div class=speakers-list>    
       <ul>
         <li><a href=#>Paul Rouget</a>
         <li><a href=#>Brian Leroux</a>
@@ -98,45 +95,64 @@ if (isset($_POST['email']) && $_POST['email'] && validEmail($_POST['email'])) {
         <li><a href=#>Paul Bakhaus</a>
       </ul>
     
-      <h3>Duke Of Yorks, Brighton, <br> 11th November 2011</h3>
-    
-      <div class=image-cube>
-        <img src=/ width= height= alt=>
-        <img src=/ width= height= alt=>
-        <img src=/ width= height= alt=>
-        <img src=/ width= height= alt=>
-        <img src=/ width= height= alt=>
-        <img src=/ width= height= alt=>
-        <img src=/ width= height= alt=>
-        <img src=/ width= height= alt=>
-        <img src=/ width= height= alt=>
-      </div>
+      <h2>Duke Of Yorks, Brighton, <br> 11th November 2011</h2>
     </div>
+    
+    <div class=image-cube>
+      <div><img src=images/home/home-1.jpg></div>
+      <div><img src=images/home/home-2.jpg></div>
+      <div><img src=images/home/home-3.jpg></div>
+      <div><img src=images/home/home-4.jpg></div>
+      <div><img src=images/home/home-5.jpg></div>
+      <div><img src=images/home/home-6.jpg></div>
+      <div><img src=images/home/home-7.jpg></div>
+      <div><img src=images/home/home-8.jpg></div>
+      <div><img src=images/home/home-9.jpg></div>
+    </div>
+    
+        <div class=tweets>
+    <?php
+    function linkify($text) {
+      // note - this order is important, i.e. links at the top, then anything else
+      $matches = array(
+        '/([A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+)/',
+        '/(^|[^\w])(#[\d\w\-]+)/',
+        '/(^|[^\w])(@([\d\w\-]+))/'
+      );
 
-    <div class=want-to>
-      <section class=speak>
-        <h1>Want to <br> speak?</h1>
-        <p>
-          Want to speak at Full Frontal 2011? We'd love to give you the platform to speak to hundreds of
-          developers and designers, so what are you waiting for, <a href="mailto:events@leftlogic.com?subject=FF2011%20Speaking%20Proposal">get in touch!</a>
-      </section>
+      $replacements = array(
+        '<a href="$1">$1</a>',
+        '$1<a href="http://search.twitter.com/search?q=$2">$2</a>',
+        '$1@<a href="http://twitter.com/$3">$3</a>'
+      );
+
+      return preg_replace($matches, $replacements, $text);
+    }
+
+
+    $favs_raw = json_decode(file_get_contents('./fullfrontalconf.json'));
+    $favs = array();
+
+    // remove old tweets
+    foreach ($favs_raw as $fav) {
+      if (stripos($fav->created_at, '2010')) {
+        array_push($favs, $fav);
+      }
+    }
+
+    shuffle($favs);
+
+    for ($i = 0; $i < 5; $i++) : $fav = $favs[$i]; ?>
+          <div><blockquote cite="http://twitter.com/<?=$fav->user->screen_name?>/statuses/<?=$fav->id_str?>">
+            <p><?=(htmlentities($fav->text))?>
+            <cite><a href="http://twitter.com/<?=$fav->user->screen_name?>"><img width=30 height=30 src="<?=$fav->user->profile_image_url?>" alt="<?=$fav->user->screen_name?>"></a><a href="http://twitter.com/<?=$fav->user->screen_name?>/statuses/<?=$fav->id_str?>"><?=$fav->user->screen_name?></a></cite>
+          </blockquote></div>
+    <?php endfor ?>
+
+          <div class=clear></div>
+        </div><!-- .tweets -->
     
-      <section class=sponsor>
-        <h1>Want to <br> sponsor?</h1>
-        <p>
-          Find out how your company can sponsor Full Frontal by reviewing our
-          <a href=/sponsorship.html>sponsorship packages</a> and <a href="mailto:events@leftlogic.com?subject=FF2011%20Sponsorship">get in touch</a> with us to discuss
-          how we can work together.
-      </section>
-    </div>
-    
-<!-- Tweets -->
-    
-    
-    
-    
-    
-    <h1 class=headline>Schedule</h1>
+    <h1 class=headline><img src=images/headlines/schedule.png alt=Schedule></h1>
     
     <ol class=schedule>
       <li>
@@ -164,7 +180,7 @@ if (isset($_POST['email']) && $_POST['email'] && validEmail($_POST['email'])) {
     
     
     
-    <h1 class=headline>Speakers</h1>
+    <h1 class=headline><img src=images/headlines/speakers.png alt=Speakers></h1>
     
     <section>
       <h1><span class=number></span>Brendan Dawes</h1>
@@ -178,7 +194,7 @@ if (isset($_POST['email']) && $_POST['email'] && validEmail($_POST['email'])) {
 
 
 
-    <h1 class=headline>Sponsors</h1>
+    <h1 class=headline><img src=images/headlines/sponsors.png alt=Sponsors></h1>
     
     <h2>
       Full Frontal
@@ -244,90 +260,92 @@ if (isset($_POST['email']) && $_POST['email'] && validEmail($_POST['email'])) {
     
     
     
-    <h1 class=headline>Workshops</h1>
+    <h1 class=headline><img src=images/headlines/workshops.png alt=Workshops></h1>
     
-    <div class=workshop>
-      <section>
+    <section class=workshop>
+      <div class=details>
         <header>
           <h1><abbr>HTML5</abbr> Javascript <abbr>API</abbr>s</h1>
-          <time>11th Novermber, 09:30 &ndash; 17:00</time>
+          <time>11th Novermber, 09:30 &ndash; 17:00</time><br>
           <a href=#>Remy Sharp</a> at <a href=#>Clarendon Centre</a>
         </header>
       
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-          dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+        <p>HTML5 has gained a lot of attention over the last 12 months. With browsers increasingly supporting the features of the vast JavaScript APIs both in and around the official HTML5 spec, it's making the job of creating awesome applications purely using these web technologies very easy indeed.
+
+        <p>This full day workshop will introduce you to HTML5 with a brief backstory, before diving into the APIs one by one. As well as going through code and showing practical demonstrations, where possible, we'll also talk about the alternatives for old browsers that don't support "awesome" out of the box. 
         
         <ul>
-          <li>Video &amps; audio &mdash; move over Flash
-          <li>Canvas &mdash; bring on the Mario games 
-          <li>Video &amps; audio &mdash; move over Flash
+          <li>Video &amp; audio – move over Flash
           <li>Canvas &mdash; bring on the Mario games
-          <li>Video &amps; audio &mdash; move over Flash
-          <li>Canvas &mdash; bring on the Mario games
-          <li>Video &amps; audio &mdash; move over Flash
+          <li>Storage &mdash; like cookies, but tastes so much better
+          <li>Offline &mdash; forget the web
+          <li>Geolocation &mdash; finders keepers
+          <li>Web Workers &mdash; multithreading for the browser
+          <li>Web Sockets &mdash; pushing data was never so easy
         </ul> 
+        
+        <h2>Who's it for?</h2>
+        
+        <p>You're not expected to have played with HTML5 just yet, but you will need to have a reasonable understanding of HTML & JavaScript. A lot of the individual APIs are being used in popular web sites today both in desktop browsers and mobile, so rest assured that this applies to developers that are working on the web today.
+        
+        <h2>Prerequisites</h2>
+        
+        <p>Delegates will need their own laptop and copies of the latest browsers - at the very least Safari 5 (Download) or Chrome 5 (Download).
 
-      </section>
+      </div>
       
-      <div>
-        <img>
-        <a href=# class="tickets early-bird">
+      <div class=callout>
+        <img src=images/workshop-speaker-1>
+        <a href=# class="buy-tickets early-bird">
           Earlybird<br>
           <span>1 day &pound;99+ <abbr>VAT</abbr></span><br>
-          <span class=button><img src=images/buy-tickets width=278 height=48 alt="Buy Tickets"></span>
+          <span class=buy-button><img src=images/buy-tickets width=278 height=48 alt="Buy Tickets"></span>
         </a>
       </div>
-    </div>
+    </section>
     
     
     
     
     
-    <h1 class=headline>The Tickets</h1>
+    <h1 class=headline><img src=images/headlines/the-tickets.png alt="The Tickets"></h1>
     
-    <section>
+    <section class=tickets>
       <h1>Conference</h1>
-      <h2>&pound;109+ <abbr>VAT</abbr> <span>No workshops</span><h2>
+      <h2>&pound;99+ <abbr>VAT</abbr> <span>No workshops</span></h2>
       <ul>
-        <li>Full Day Conference Tickets
-        <li>Complimenatary Coffer &amps; Snacks
+        <li>Full Day Conference Ticket
+        <li>Complimenatary Coffer &amp; Snacks
         <li>Access to the After Party
       </ul>
-      <span>Sold Out!</span>
+      <span class=sold-button><img src=images/sold-out.png width=278 height=48 alt="Sold Out"></span>
     </section> 
     
-    <section>
-      <h1>Conference</h1>
-      <h2>&pound;109+ <abbr>VAT</abbr> <span>No workshops</span><h2>
+    <section class=tickets>
+      <h1>Workshop 1</h1>
+      <h2>&pound;109+ <abbr>VAT</abbr> <span>Inc. Confernece</span></h2>
       <ul>
-        <li>Full Day Conference Tickets
-        <li>Complimentary Coffee &amps; Snacks
+        <li><abbr>HTML5</abbr> Javascript <abbr>API</abbr> Workshop
+        <li>Full Day Conference Ticket
+        <li>Complimentary Coffee &amp; Snacks
         <li>Access to the After Party
+        <li>Free <em>Swag</em> Bag
       </ul>
-      <span>Sold Out!</span
+      <span class=sold-button><img src=images/sold-out.png width=278 height=48 alt="Sold Out"></span>
     </section>
     
-    <section>
-      <h1>Conference</h1>
-      <h2>&pound;109+ <abbr>VAT</abbr> <span>No workshops</span><h2>
+    <section class=tickets>
+      <h1>Workshop 2</h1>
+      <h2>&pound;109+ <abbr>VAT</abbr> <span>No workshops</span></h2>
       <ul>
         <li>Full Day Conference Tickets
-        <li>Complimenatary Coffer &amps; Snacks
+        <li>Complimenatary Coffer &amp; Snacks
         <li>Access to the After Party
       </ul>
-      <a href=#>Buy Tickets</a>
+      <a href=# class=buy-button><img src=images/buy-tickets.png width=278 height=48 alt="Buy Tickets"></a>
     </section>
     
-    <p>
+    <p class="highlight clear">
       Please note that due to the was Stage<abbr>HQ</abbr> (our payment system) works, the <abbr>VAT</abbr>
       won't show up separately when buying the tickets (nor in PayPal). <abbr>VAT</abbr> has been separately
       added, and our <abbr>VAT</abbr> registration is: 993 1266 95
