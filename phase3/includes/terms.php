d3a9a7d969dd73fb17ed7773c72c798a3c7f39de
remy <remy@remysharp.com>
2011-07-01 10:28:28 +0100

    +terms

---
+++ phase3/includes/terms.php
@@ -0,0 +1,16 @@
+<h2>Transfer of Ticket</h2>
+
+<p>If you wish to transfer your ticket to a friend or colleague you must do so in writing at least 1 month before the date of the event. No transfers can be issued after this date and the ticket cannot be refunded.
+
+<h2>Rights</h2>
+
+<p>You grant us the right to use any photographs or quotes taken during the event for any future marketing.
+
+<h2>Privacy</h2>
+
+<p>Your personal information will be protected and we will not share this with any other business. The only exception to this is where we are legally obliged to by court order or legal process served to this site.
+
+<h2>Contact</h2>
+
+<p>Left Logic (company number 05926047) can be contacted via email: <a href="mailto:events@leftlogic.com">events@leftlogic.com</a> or by post, at: Left Logic, 44 Burstead Close, Brighton, BN1 7HT, UK.
+
