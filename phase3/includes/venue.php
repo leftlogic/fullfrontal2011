9d77c1970347c4f20a857b9cafbd7df1fb07116e
remy <remy@remysharp.com>
2011-07-01 14:41:59 +0100

    venue hover js

--- phase3/includes/venue.php
+++ phase3/includes/venue.php
@@ -21,12 +21,12 @@
   
 </section>
 
-<section class=associated-venues>
+<section class=associated-venues id=associated-venues>
   <div class=details>
     
     <h2>Hotels</h2>
     <ul>
-      <li data-latlng=50.83283,-0.140247 id=1><span class=venue-1>1</span>
+      <li data-latlng=50.831386981520524,-0.14038681983947754><span class=venue-1>1</span>
           <div>
             <h3><a href=http://brightonhotels.jurysinns.com>Jury's Inn'</a> £112*</h3>
             <p>
@@ -34,14 +34,14 @@
               <small>* 2 nights special Full Frontal, £129 also includes breakfast</small>
           </div>
           
-      <li data-latlng=50.83321,-0.140119 data-id=2><span class=venue-2>2</span>
+      <li data-latlng=50.83632670343803,-0.1439809799194336><span class=venue-2>2</span>
           <div>
             <h3><a href=http://www.travelodge.co.uk/hotels/info?hotelId=18>Travelodge</a> £38</h3>
             <p>
               165-167 Preston Road, BN1 6AU<br>
           </div>
           
-      <li data-latlng=50.829361,-0.139518 data-id=3><span class=venue-3>3</span>
+      <li data-latlng=50.829361,-0.139518><span class=venue-3>3</span>
           <div>
             <h3><a href=http://www.myhotels.com/my-hotel-brighton/index.html>My Hotel</a> £95, £99*</h3>
             <p>
@@ -49,7 +49,7 @@
               <small>* £95 for Thurs, £99 for Fri</small>
           </div>
           
-      <li data-latlng=50.825159,-0.14102 data-id=4><span class=venue-4>4</span>
+      <li data-latlng=50.825159,-0.14102><span class=venue-4>4</span>
           <div>
             <h3><a href=http://www.barcelo-hotels.co.uk/hotels/southern-england/barcelo-brighton-old-ship-hotel>Old Ship hotel</a> £63/£73*</h3>
             <p>
@@ -60,7 +60,7 @@
 
     <h2>Food</h2>
     <ul class=food>
-      <li data-latlng=50.835473,-0.140913 data-id=5><span class=venue-5>5</span>
+      <li data-latlng=50.835473,-0.140913><span class=venue-5>5</span>
           <div>
             <h3>Circus Circus <em>Pub</em></h3>
             <p>101 Stroundly Road, BN1 4DJ
@@ -72,19 +72,19 @@
             <p>165-167 Preston Road, BN1 6AU
           </div>
           
-      <li data-latlng=50.834362,-0.136814 data-id=7><span class=venue-7>7</span>
+      <li data-latlng=50.834362,-0.136814><span class=venue-7>7</span>
           <div>
             <h3>Bardsley's' <em>Fish &amp; Chips</em></h3>
             <p>17 Jubilee Sreet, BN1 1GE
           </div>
           
-      <li data-latlng=50.834362,-0.136814 data-id=8><span class=venue-8>8</span>
+      <li data-latlng=50.834362,-0.136814><span class=venue-8>8</span>
           <div>
             <h3>Mitre Tavern <em>Pub</em></h3>
             <p>Kings Road, BN1 2GS
           </div>
       
-      <li data-latlng=50.834029696136774,-0.13861119747161865 data-id=9><span class=venue-9>9</span>
+      <li data-latlng=50.834029696136774,-0.13861119747161865><span class=venue-9>9</span>
           <div>
             <h3>Mediterrane <em>Pub</em></h3>
             <p>Next to Duke of York's
--- phase3/js/map.js
+++ phase3/js/map.js
@@ -1,60 +1,66 @@
-var map = new google.maps.Map(document.getElementById('venue-map'), {
-  center: new google.maps.LatLng(51, 0),
-  zoom: 6,
-  mapTypeId: google.maps.MapTypeId.ROADMAP,
-  disableDefaultUI: true
-});
+function newHoverIconAction(el, marker, standardIcon, hoverIcon) {
+  return function (event) {
+    console.log(event, arguments);
+    if (event.type === 'mouseover') {
+      marker.setIcon(hoverIcon);
+      el.setAttribute('class','selected');  
+    } else {
+      marker.setIcon(standardIcon);
+      el.removeAttribute('class');
+    }
+  };
+}
 
+// DOY: 50.8336812, -0.1388816
 
-var standardIcon =  new google.maps.MarkerImage(
- '/images/map-markers.png',
- new google.maps.Size(29, 39, 'px', 'px'),
- new google.maps.Point(0,0),
- new google.maps.Point(16, 39)
-);
+var iconSize = new google.maps.Size(29, 39, 'px', 'px'),
+    iconPoint = new google.maps.Point(16, 39),
+    iconURL = '/images/map-markers.png',
+    iconWidth = 29,
+    map = new google.maps.Map(document.getElementById('venue-map'), {
+      center: new google.maps.LatLng(50.8339238, -0.1385427),
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true
+    }),
+    lis = document.getElementById('associated-venues').getElementsByTagName('li'),
+    len = lis.length;
+    
+for (var i = 0; i < len; i++) {
+  !function (i) {
+    var el = lis[i],
+        latlng = el.getAttribute('data-latlng').split(','),
+        standardIcon = new google.maps.MarkerImage(
+          iconURL,
+          iconSize,
+          new google.maps.Point(i * iconWidth, 0),
+          iconPoint
+        ),
+        hoverIcon = new google.maps.MarkerImage(
+          iconURL,
+          iconSize,
+          new google.maps.Point(i * iconWidth, 39),
+          iconPoint
+        ),
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(latlng[0], latlng[1]),
+          flat: true,
+          icon: standardIcon
+        }),
+        hoverIconAction = newHoverIconAction(el, marker, standardIcon, hoverIcon);
 
-var hoverIcon =  new google.maps.MarkerImage(
- '/images/map-markers.png',
- new google.maps.Size(29, 39, 'px', 'px'),
- new google.maps.Point(0,39),
- new google.maps.Point(16, 39)
-);
-
-
-var venue1 = document.getElementById('1');
-
-var marker1 = new google.maps.Marker({
-  position: new google.maps.LatLng(51.1, 0),
-  flat: true,
-  icon: standardIcon
-});
-
-
-marker1.setMap(map);
-
-google.maps.event.addListener(marker1, 'mouseover', function() {
-  hoverIconAction(true);
-});
-
-google.maps.event.addListener(marker1, 'mouseout', function() {
-  hoverIconAction(false);
-});
-
-var hoverIconAction = function (hover) {
-  if (hover){
-    marker1.setIcon(hoverIcon);
-    venue1.setAttribute('class','selected');  
-  } else {
-    marker1.setIcon(standardIcon)    
-    venue1.setAttribute('class','');
-  }
+    // event handlers - sweeeeeeet HAWT ::rasp::
+    google.maps.event.addListener(marker, 'mouseover', function () {
+      hoverIconAction({ type: 'mouseover' });
+    });
+    google.maps.event.addListener(marker, 'mouseout', function () {
+      hoverIconAction({ type: 'mouseout' });
+    });
+    el.onmouseover = hoverIconAction;
+    el.onmouseout = hoverIconAction;
+    
+    marker.setMap(map);
+  }(i);
 }
 
 
-
-venue1.onmouseover = function () {
-  hoverIconAction(true);
-}
-venue1.onmouseout = function () {
-  hoverIconAction(false);
-}
