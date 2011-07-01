1ac13fc234e72fa0d3909229688c3eec4225a2a7
remy <remy@remysharp.com>
2011-07-01 13:04:59 +0100

    latlng for food

--- phase3/includes/venue.php
+++ phase3/includes/venue.php
@@ -26,32 +26,32 @@
     
     <h2>Hotels</h2>
     <ul>
-      <li><span class=venue-1>1</span>
+      <li data-latlng=50.83283,-0.140247 data-id=1><span class=venue-1>1</span>
           <div>
-            <h3><a href=http://brightonhotels.jurysinns.com data-latlng=50.83283,-0.140247>Jury's Inn'</a> £112*</h3>
+            <h3><a href=http://brightonhotels.jurysinns.com>Jury's Inn'</a> £112*</h3>
             <p>
               101 Stroundly Road, BN1 4DJ<br>
               <small>* 2 nights special Full Frontal, £129 also includes breakfast</small>
           </div>
           
-      <li><span class=venue-2>2</span>
+      <li data-latlng=50.83321,-0.140119 data-id=2><span class=venue-2>2</span>
           <div>
-            <h3><a href=http://www.travelodge.co.uk/hotels/info?hotelId=18 data-latlng=50.83321,-0.140119>Travelodge</a> £38</h3>
+            <h3><a href=http://www.travelodge.co.uk/hotels/info?hotelId=18>Travelodge</a> £38</h3>
             <p>
               165-167 Preston Road, BN1 6AU<br>
           </div>
           
-      <li><span class=venue-3>3</span>
+      <li data-latlng=50.829361,-0.139518 data-id=3><span class=venue-3>3</span>
           <div>
-            <h3><a href=http://www.myhotels.com/my-hotel-brighton/index.html data-latlng=50.829361,-0.139518>My Hotel</a> £95, £99*</h3>
+            <h3><a href=http://www.myhotels.com/my-hotel-brighton/index.html>My Hotel</a> £95, £99*</h3>
             <p>
               17 Jubilee St, BN1 1GE<br>
               <small>* £95 for Thurs, £99 for Fri</small>
           </div>
           
-      <li><span class=venue-4>4</span>
+      <li data-latlng=50.825159,-0.14102 data-id=4><span class=venue-4>4</span>
           <div>
-            <h3><a href=http://www.barcelo-hotels.co.uk/hotels/southern-england/barcelo-brighton-old-ship-hotel data-latlng=50.825159,-0.14102>Old Ship hotel</a> £63/£73*</h3>
+            <h3><a href=http://www.barcelo-hotels.co.uk/hotels/southern-england/barcelo-brighton-old-ship-hotel>Old Ship hotel</a> £63/£73*</h3>
             <p>
               King's Road, BN1 1NR<br>
               <small>* £73 includes breakfast</small>
@@ -60,41 +60,39 @@
 
     <h2>Food</h2>
     <ul class=food>
-      <li><span class=venue-5>5</span>
+      <li data-latlng=50.835473,-0.140913 data-id=5><span class=venue-5>5</span>
           <div>
             <h3>Circus Circus <em>Pub</em></h3>
             <p>101 Stroundly Road, BN1 4DJ
           </div>
           
-      <li><span class=venue-6>6</span>
+      <li data-latlng=50.836815,-0.138917 data-it=6><span class=venue-6>6</span>
           <div>
             <h3>The Open House <em>Pub</em></h3>
             <p>165-167 Preston Road, BN1 6AU
           </div>
           
-      <li><span class=venue-7>7</span>
+      <li data-latlng=50.834362,-0.136814 data-id=7><span class=venue-7>7</span>
           <div>
             <h3>Bardsley's' <em>Fish &amp; Chips</em></h3>
             <p>17 Jubilee Sreet, BN1 1GE
           </div>
           
-      <li><span class=venue-8>8</span>
+      <li data-latlng=50.834362,-0.136814 data-id=8><span class=venue-8>8</span>
           <div>
-            <h3>Mitre Travern <em>Pub</em></h3>
+            <h3>Mitre Tavern <em>Pub</em></h3>
             <p>Kings Road, BN1 2GS
           </div>
       
-      <li><span class=venue-9>9</span>
+      <li data-latlng=50.834029696136774,-0.13861119747161865 data-id=9><span class=venue-9>9</span>
           <div>
-            <h3>Mitre Travern <em>Pub</em></h3>
+            <h3>Mediterrane <em>Pub</em></h3>
             <p>Next to Duke of York's
           </div>
     </ul>
 
     <h2>After Party</h2>
-    <p>
-      Please join us at the Full Frontal After Party at <a href#>Oh So Social</a> from
-      6:30pm - 1am where <a href=#>Mozilla</a>, our Full Monty sponsor, had bought you a pint!
+    <p>More info coming soon, <a href=http://twitter.com/fullfrontalconf>follow @fullfrontalconf for the latest info</a>.
 
         
   </div>
