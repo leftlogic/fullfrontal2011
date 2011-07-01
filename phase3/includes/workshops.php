dca9abc3fbb045de53bd9d1db1a093229c634ee9
remy <remy@remysharp.com>
2011-07-01 12:18:39 +0100

    Speakers on workshop

--- phase3/includes/workshops.php
+++ phase3/includes/workshops.php
@@ -1,40 +1,81 @@
 <section class=workshop>
   <div class=details>
     <header>
-      <h1><abbr>HTML5</abbr> Javascript <abbr>API</abbr>s</h1>
-      <time>11th Novermber, 09:30 &ndash; 17:00</time><br>
-      <a href=#>Remy Sharp</a> at <a href=#>Clarendon Centre</a>
+      <h1>Node &amp; <abbr>HTML5</abbr> for a real-time web</h1>
+      <time>9th Novermber, 09:30 &ndash; 17:00</time><br>
+      <a href=/speakers#remy>Remy Sharp</a> at The Lighthouse</a>
     </header>
   
-    <p>HTML5 has gained a lot of attention over the last 12 months. With browsers increasingly supporting the features of the vast JavaScript APIs both in and around the official HTML5 spec, it's making the job of creating awesome applications purely using these web technologies very easy indeed.
+    <p>JavaScript is increasingly seen as the ubiquitous language for developing for the web, so it stands to reason that JavaScript be used for the server side too.
 
-    <p>This full day workshop will introduce you to HTML5 with a brief backstory, before diving into the APIs one by one. As well as going through code and showing practical demonstrations, where possible, we'll also talk about the alternatives for old browsers that don't support "awesome" out of the box. 
-    
+    <p>Node allows for applications to be developed in JavaScript and is running on the incredibly fast V8 engine (the same JavaScript engine used to run Google Chrome). It can handle thousands of concurrent connections due to it’s event model, and because of this event model, developing for Node is very familiar if you’ve already written JavaScript for the browser - as this the same event model.
+
+    <h2>What will be covered</h2>
     <ul>
-      <li>Video &amp; audio – move over Flash
-      <li>Canvas &mdash; bring on the Mario games
-      <li>Storage &mdash; like cookies, but tastes so much better
-      <li>Offline &mdash; forget the web
-      <li>Geolocation &mdash; finders keepers
-      <li>Web Workers &mdash; multithreading for the browser
-      <li>Web Sockets &mdash; pushing data was never so easy
+      <li>The basics: Async development, modules, requires, exports and scope
+      <li>Building web servers: by hand, with Connect, with Express.js
+      <li>Working with document databases (mongodb &amp; redis)
+      <li>Going real time: WebSockets and Socket.IO
+      <li>Releasing and deploying: npm, cluster
     </ul> 
     
-    <h2>Who's it for?</h2>
+    <h2>What do you need for this workshop?</h2>
     
-    <p>You're not expected to have played with HTML5 just yet, but you will need to have a reasonable understanding of HTML & JavaScript. A lot of the individual APIs are being used in popular web sites today both in desktop browsers and mobile, so rest assured that this applies to developers that are working on the web today.
-    
-    <h2>Prerequisites</h2>
-    
-    <p>Delegates will need their own laptop and copies of the latest browsers - at the very least Safari 5 (Download) or Chrome 5 (Download).
+    <ul>
+      <li><a href="https://github.com/joyent/node/wiki/Installation">Node installed</a> on your machine (0.4.0 is currently the latest stable release) (<a href="http://blog.tatham.oddie.com.au/2011/03/16/node-js-on-windows/">also available on Windows</a>)
+      <li>Ideally have <a href="http://help.github.com/git-installation-redirect/">git installed</a>
+      <li>A text editor (Vim, TextMate, Coda, Textpad++, etc)
+      <li>A browser with latest technology baked in, like <a href="http://www.google.com/chrome">Google Chrome</a> for instance
+    </ul>
+
 
   </div>
   
   <div class=callout>
-    <img src=images/pictures/workshop-speaker-1>
-    <a href=# class="buy-tickets early-bird">
-      Earlybird<br>
-      <span>1 day &pound;99+ <abbr>VAT</abbr></span><br>
+    <img src=images/speakers/remy.jpg>
+    <a href=/tickets class="buy-tickets early-bird">
+      Both days<br>
+      <span>&pound;595+ <abbr>VAT</abbr></span><br>
+      <span class=buy-button><img src=images/buy-tickets width=278 height=48 alt="Buy Tickets"></span>
+    </a>
+    <a href=/tickets class="buy-tickets early-bird">
+      Day 1<br>
+      <span>&pound;345+ <abbr>VAT</abbr></span><br>
+      <span class=buy-button><img src=images/buy-tickets width=278 height=48 alt="Buy Tickets"></span>
+    </a>
+    <a href=/tickets class="buy-tickets early-bird">
+      Day 2<br>
+      <span>&pound;345+ <abbr>VAT</abbr></span><br>
+      <span class=buy-button><img src=images/buy-tickets width=278 height=48 alt="Buy Tickets"></span>
+    </a>
+  </div>
+</section>
+
+<section class=workshop>
+  <div class=details>
+    <header>
+      <h1>Mobilism</h1>
+      <time>9th Novermber, 09:30 &ndash; 17:00</time><br>
+      <a href=/speakers#ppk>PPK</a> at The Lighthouse</a>
+    </header>
+  
+    <p>Web developers are enthusiastic about mobile, but need some background information as well as a chance to practice their skills on as many phones as possible. Therefore PPK will be running this workshop to teach you what you need to know.
+  
+    <p>We found that a one-day workshop is too short. By the time the first afternoon ends you've just started to understand what it's all about, and you're beginning to get some ideas for future experiments. Besides, you want to talk to your fellow attendees about practical problems and phones. Therefore our workshop is a two-day affair, and we'll also have dinner together at the end of day 1 (plus maybe a few beers, if you're so inclined).
+
+    <p>In general, Stephen focuses more on the CSS and design side of things, while PPK handles general information and JavaScript. Both of us will talk for a while about fundamental principles, issues, tips and tricks, and curious facts of the mobile web.
+
+    <p>Still, we are not big believers in workshops where the teacher talks for eight hours straight. Therefore the idea is that you take a genuine project you have to work on for a genuine client, or, failing that, a mobile experiment you’re working on. We'll discuss these practical examples, you'll work on them during the workshop, and we'll solve practical problems together.
+
+    <p>We will take plenty of test phones, including a few obscure ones such as the Nokia N900 (Maemo), the HTC Smart (Brew MP), or a low-end LG Android, that we can try stuff on. If you have them you should also take a few phones, and not only iPhones or Androids. That allows you and your fellow attendees to test your websites on browsers and operating systems you've never even heard of. (Did we mention how exciting the mobile web is?)
+    
+  </div>
+  
+  <div class=callout>
+    <img src=images/speakers/ppk.jpg>
+    <a href=/tickets class="buy-tickets early-bird">
+      Mobilism<br>
+      <span>&pound;595+ <abbr>VAT</abbr></span><br>
       <span class=buy-button><img src=images/buy-tickets width=278 height=48 alt="Buy Tickets"></span>
     </a>
   </div>
