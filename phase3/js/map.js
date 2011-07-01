1bcc4c5338acae40962d8802fca6af7397db55e1
Jon Linklater-Johnson <jon@binarytales.co.uk>
2011-07-01 13:29:30 +0100

    Link venue list with map marker (first venue only, hard coded)

--- phase3/js/map.js
+++ phase3/js/map.js
@@ -5,21 +5,56 @@ var map = new google.maps.Map(document.getElementById('venue-map'), {
   disableDefaultUI: true
 });
 
-var standardMarker = {
-  location: '/images/map-markers.png',
-  size: new google.maps.Size(29, 39, 'px', 'px'),
-  anchor: new google.maps.Point(16, 39)
-}
 
-var marker = new google.maps.Marker({
+var standardIcon =  new google.maps.MarkerImage(
+ '/images/map-markers.png',
+ new google.maps.Size(29, 39, 'px', 'px'),
+ new google.maps.Point(0,0),
+ new google.maps.Point(16, 39)
+);
+
+var hoverIcon =  new google.maps.MarkerImage(
+ '/images/map-markers.png',
+ new google.maps.Size(29, 39, 'px', 'px'),
+ new google.maps.Point(0,39),
+ new google.maps.Point(16, 39)
+);
+
+
+var venue1 = document.getElementById('1');
+
+var marker1 = new google.maps.Marker({
   position: new google.maps.LatLng(51.1, 0),
   flat: true,
-  icon: new google.maps.MarkerImage(
-    standardMarker.location,
-    standardMarker.size,
-    new google.maps.Point(0,0),
-    standardMarker.anchor
-  )
+  icon: standardIcon
 });
 
-marker.setMap(map);
+
+marker1.setMap(map);
+
+google.maps.event.addListener(marker1, 'mouseover', function() {
+  hoverIconAction(true);
+});
+
+google.maps.event.addListener(marker1, 'mouseout', function() {
+  hoverIconAction(false);
+});
+
+var hoverIconAction = function (hover) {
+  if (hover){
+    marker1.setIcon(hoverIcon);
+    venue1.setAttribute('class','selected');  
+  } else {
+    marker1.setIcon(standardIcon)    
+    venue1.setAttribute('class','');
+  }
+}
+
+
+
+venue1.onmouseover = function () {
+  hoverIconAction(true);
+}
+venue1.onmouseout = function () {
+  hoverIconAction(false);
+}
