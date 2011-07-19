791d53e685198e1dd1d5738de9d65c7ff67a58d9
remy <remy@remysharp.com>
2011-07-19 03:03:41 +0100

    Randomising order of workshops - just to give a bit more variation

---
+++ phase3/includes/workshop-remy.php
@@ -0,0 +1,59 @@
+<section id=node class=workshop>
+  <div class=details>
+    <header>
+      <h1>Node &amp; <abbr>HTML5</abbr> for a real-time web</h1>
+      <time>9th &amp; 10th Novermber, 09:30 &ndash; 17:00</time><br>
+      <a href=/speakers#remy>Remy Sharp</a> at The Lighthouse</a>
+    </header>
+  
+    <p>JavaScript is increasingly seen as the ubiquitous language for developing for the web, so it stands to reason that JavaScript be used for the server side too.
+
+    <p>Node allows for applications to be developed in JavaScript and is running on the incredibly fast V8 engine (the same JavaScript engine used to run Google Chrome). It can handle thousands of concurrent connections due to it’s event model, and because of this event model, developing for Node is very familiar if you’ve already written JavaScript for the browser - as this the same event model.
+
+    <p>There are three ticket types available:
+    <ul>
+      <li>Both days @ £595+VAT (evening meal included on Wednesday)</li>
+      <li>Introduction to Node - one day @ £345+VAT</li>
+      <li>Real-time: using HTML5 with node - one day @ £345+VAT</li>
+    </ul>
+    <p>All ticket types include a full pass to the Friday conference day event.</p>
+
+    <h2>Introduction to Node: Day 1</h2>
+    <ul>
+      <li>The basics: Async development, modules, requires, exports and scope
+      <li>Building web servers: by hand, with Connect, with Express.js
+      <li>Working with document databases (mongodb &amp; redis)
+      <li>Repo control, releasing and deploying using npm
+    </ul> 
+
+    <h2>Using <abbr title="Technically speaking, we won't be using *any* HTML5, they're all non-HTML5 specs, but, well, it reads as a better title, you know?">HTML5</abbr> tech with node: Day 2</h2>
+    <ul>
+      <li>Going real time: WebSockets and Socket.IO
+      <li>Building WebSockets from scratch and using Socket.IO
+      <li>Using WebSockets in the browser, with and without support
+      <li>Server sent events and long polling: building the server and clients
+    </ul> 
+    
+    <h2>What do you need for this workshop?</h2>
+    
+    <ul>
+      <li><a href="https://github.com/joyent/node/wiki/Installation">Node installed</a> on your machine (0.4.0 is currently the latest stable release) (<a href="http://blog.tatham.oddie.com.au/2011/03/16/node-js-on-windows/">also available on Windows</a>)
+      <li>Ideally have <a href="http://help.github.com/git-installation-redirect/">git installed</a>
+      <li>A text editor (Vim, TextMate, Coda, Textpad++, etc)
+      <li>A browser with latest technology baked in, like <a href="http://www.google.com/chrome">Google Chrome</a> for instance
+    </ul>
+
+
+  </div>
+  
+  <div class=callout>
+    <img class=headshot src=images/speakers/remy.jpg>
+    <a href=http://leftlogic.stagehq.com/events/901/booking/new class="buy-tickets" style="font-size: 3em;">
+      Single day @ <span>&pound;345+ <abbr>VAT</abbr></span> <small>or</small><br>
+      Both days @ <span>&pound;595+ <abbr>VAT</abbr></span>
+      <span class=buy-button><img src=images/buy-tickets.png width=278 height=48 alt="Buy Tickets"></span>
+    </a>
+    <p>Ticket includes pass to Friday event.<br>Two day ticket includes evening meal on Wednesday with Remy &amp; PPK.<br>
+      Tea, Coffee and lunch provided for both days.
+  </div>
+</section>
