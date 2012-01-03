fe78281a48dfb2bde166bba2b137ec910eccced7
Remy Sharp <remy@remysharp.com>
2012-01-03 12:55:36 +0000

    added the audio links to sessions

--- phase3/includes/schedule.php
+++ phase3/includes/schedule.php
@@ -30,6 +30,7 @@
       <td>
         <h1>CoffeeScript Design Decisions</h1>
         <p>Although limited by what's possible to express in simple JavaScript, CoffeeScript tries to provide pieces of clear syntax that fit together in harmony. We'll dive into the rationale behind some of CoffeeScript's language choices to look at the trade offs involved, and both the why's and why not's.
+        <p><a class="audio" href="/audio/ff2011-01-coffeescript.mp3">Listen to Jeremy's session</a></p>
       </td>
     </tr>
     <tr>
@@ -45,6 +46,7 @@ by JavaScript and the ever increasing rendering capabilities of modern
 browsers, but are we getting these at the expense of the Web?  This
 talk will explore the good, the bad, and the fugly of rich interfaces,
 while examining how and why we should take care not to damage the Web.
+        <p><a class="audio" href="/audio/ff2011-02-excessive.mp3">Listen to Phil's session</a></p>
       </td>
     </tr>
     <tr class=info>
@@ -63,6 +65,7 @@ while examining how and why we should take care not to damage the Web.
       <td>
         <h1>Respectable code-editing in the browser</h1>
         <p>The past few years have seen the rise and maturation of several full-blown in-browser code editors: CodeMirror, ACE (Cloud9), and Orion. No longer must we mortify users with huge, clunky text areas when they need to edit some CSS or script. This talk will go over the current state of JavaScript-based editors, and describe the implementation of CodeMirror in some depth.
+        <p><a class="audio" href="/audio/ff2011-03-respectful-code-editing.mp3">Listen to Marijn's session</a></p>
       </td>
     </tr>
     <tr>
@@ -76,6 +79,7 @@ while examining how and why we should take care not to damage the Web.
         <p>Building and scaling a web based IDE is a big challenge, and doing this on Node.JS gave us a lot of unique challenges.
 Cloud9 is built using a full Ajax client, and besides Node.JS utilizes a full set of new HTML5 features and offline support.
 In this talk Rik will go through a quick demo of Cloud9 IDE, and then into the unique architecture and design decisions that we made to build Cloud9 IDE. This will be of interest to medium to advanced level programmers, who want to know more about Cloud9 IDE, Node.JS and scalable app development.
+        <p><a class="audio" href="/audio/ff2011-04-cloud9-on-node.mp3">Listen to Rik's session</a></p>
       </td>
     </tr>
     <tr class=info>
@@ -96,6 +100,7 @@ In this talk Rik will go through a quick demo of Cloud9 IDE, and then into the u
         <p>Building large web applications with dozens of developers is a difficult task.  Organising the engineers around a common goal is one thing, but organising your code so that people can work efficiently is another. Many large applications suffer from growing pains after just a few months in production due to poorly designed JavaScript with unclear upgrade and extension paths.
 Learn the tips, tricks, and techniques that allowed large sites such as My Yahoo! and the Yahoo! homepage to continue to grow, scale, and change over time without throwing away previous work.
 This talk isn’t specific to any JavaScript library, rather, it gives you new ways to apply the libraries you’re already using. The principles of good, loosely-coupled design apply to any system, and you’ll learn how this can help your application today.
+        <p><a class="audio" href="/audio/ff2011-05-scalable-apps.mp3">Listen to Nicholas' session</a></p>
       </td>
     </tr>
      <tr>
@@ -107,6 +112,7 @@ This talk isn’t specific to any JavaScript library, rather, it gives you new w
       <td>
         <h1>Beyond the page</h1>
         <p>The latest browser APIs now make it possible to redesign how your web pages interact with other applications. Web pages are too often little islands that fail to play well with the wider user interfaces of our devices. This talk will explore the possibilities from Drag and Drop to Web Intents, demonstrating how to make web pages more equal in the world of applications.
+        <p><a class="audio" href="/audio/ff2011-06-beyond-the-page.mp3">Listen to Glenn's session</a></p>
       </td>
     </tr>
 
@@ -128,6 +134,7 @@ This talk isn’t specific to any JavaScript library, rather, it gives you new w
         <h1>Beyond The Planet Of The Geeks</h1>
         <p>Brendan Dawes is a big-a-geek as anyone; he loves nothing more than making and experimenting with all the wondrous technologies, tools, toys and other magical things that constantly surround us. But the thing is, geeks never changed anything, well not in a real-world sense. Making cutting edge Javascript demos with the likes of Canvas or SVG are all well and good but for things to really change and have an impact stuff needs to move beyond the confines of the world of the geek and become common place, the norm and paradoxically, invisible!
         <p>In this session Brendan takes you through his process of experimentation with purpose and how he and the team at magneticNorth are now actively using these exciting new technolgies on real client work that goes beyond *bouncing ball* demoes to create new interfaces and new ways to explore.
+        <p><a class="audio" href="/audio/ff2011-07-beyond-the-planet-of-geeks.mp3">Listen to Brendan's session</a></p>
       </td>
     </tr>
 
@@ -140,6 +147,7 @@ This talk isn’t specific to any JavaScript library, rather, it gives you new w
       <td>
         <h1>You gotta do what you gotta do</h1>
         <p>HTML is incredible and powerful. HTML is a mess. Marcin, a user experience designer at Google, will talk about the experience working on interactive doodles like Pac-Man, Jules Verne and Les Paul. How do we put together things that will be seen by hundreds millions of people? What did we get right? What did we learn the hard way? What rules were broken and what contraptions put together? (Oh, and make sure to bring your phones!)
+        <p><a class="audio" href="/audio/ff2011-08-you-gotta-do.mp3">Listen to Marcin's session</a></p>
       </td>
     </tr>
     
